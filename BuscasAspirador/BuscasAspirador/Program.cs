c14e07684a77b64365abc7817211c0317c5d5308
Arthur <contato.arthurfr@gmail.com>
2017-10-05 17:02:16 -0300

    Menu da função main

--- BuscasAspirador/BuscasAspirador/Program.cs
+++ BuscasAspirador/BuscasAspirador/Program.cs
@@ -8,17 +8,51 @@ namespace BuscasAspirador
 		public static void Main(string[] args)
 		{
             Estado objetivo;
+            string opcao;
             Agente agente = new Agente(1);
-            objetivo = agente.BuscaEstrela();
-            Console.WriteLine(MostraCaminho(objetivo,0));
-            Console.ReadLine();
+                
+            try
+            {
+                do
+                {
+                    Menu();
 
-            agente = new Agente(1);
-            objetivo = agente.BuscaLargura();
-            Console.WriteLine(MostraCaminho(objetivo, 0));
-            Console.ReadLine();
+                    opcao = Console.ReadLine();
+                    switch (opcao)
+                    {
+                        case "1"://BuscaLargura
+                            objetivo = agente.BuscaLargura();
+                            Console.WriteLine(MostraCaminho(objetivo, 0));
+                            break;
+                        case "2"://BuscaProfunda
+                            //objetivo = agente.BuscaProfunda();
+                            //Console.WriteLine(MostraCaminho(objetivo));
+                            break;
+                        case "3"://BuscaEstrela
+                                 objetivo = agente.BuscaEstrela();
+                                 Console.WriteLine(MostraCaminho(objetivo,0));
+                            break;
+
+                    } //Fim switch
+                } while (opcao != "0");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
         }
 
+        public static void Menu()
+        {
+            Console.WriteLine("**MENU**");
+            Console.WriteLine("1. BuscaLargura");
+            Console.WriteLine("2. BuscaProfunda");
+            Console.WriteLine("3. Altera BuscaEstrela");
+            Console.WriteLine("0. Sair");
+            Console.WriteLine("Opcao: ");
+        }
 
         public static string MostraCaminho(Estado objetivo, int profundidade)
         {
