8be12205f9bc2e011d966d7240789d201f13291f
Arthur <contato.arthurfr@gmail.com>
2017-11-08 22:22:27 -0200

    base

---
+++ MiniMax/MiniMax/Jogo/Jogo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniMax.Jogo
+{
+    class Jogo
+    {
+        No no;
+
+        public Jogo(string[,] estadoInicial)
+        {
+            this.no.setEstado(estadoInicial);
+        }
+
+        public string[,] Sucessor(string[,] estado, Tuple<int, int, string> acao)
+        {
+
+            return null;
+        }
+    }
+}
---
+++ MiniMax/MiniMax/Jogo/No.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniMax.Jogo
+{
+    class No
+    {
+        string[,] estado;
+
+        public void setEstado(string[,] estado)
+        {
+            this.estado = estado;
+        }
+    }
+}
